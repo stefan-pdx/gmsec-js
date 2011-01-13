000036aca3a5bf2476ed77f11dd117929e1a97d5
Stefan Novak <stefan.louis.novak@gmail.com>
2011-01-13 08:34:52 -0500

    no longer need GMSEC includes in project folder

--- server/include/framework/Status.h
+++
@@ -1,6 +0,0 @@
-
-/* Copyright 2007-2010 United States Government as represented by the     */
-/* Administrator of The National Aeronautics and Space Administration. All */
-/* Rights Reserved.                                                        */
-
-#include <gmsec/Status.h>
